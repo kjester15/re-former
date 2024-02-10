416bfed6f7fa96936eaf04bbc57310f5a32497d7
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-02-09 16:01:40 -0800

    Add #edit and #update to Users controller, and create edit.html.erb view with appropriate routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
@@ -10,6 +11,20 @@ class UsersController < ApplicationController
       redirect_to new_user_path
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
